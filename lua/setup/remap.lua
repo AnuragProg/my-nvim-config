5d98c6f1397b1588e27648902234ec02bb597a36
Anurag Singh <anuragcollege7@gmail.com>
2024-06-15 13:03:25 +0545

    added remap for moving visual selected text up and down

--- lua/setup/remap.lua
+++ lua/setup/remap.lua
@@ -7,9 +7,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {desc='LSP: Code Action'})
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {desc='LSP: Code Action'})
 
--- for go to definitions
 
-
+-- visual shifts up and down in visual mode
+vim.keymap.set("v", "<C-j>", ":m'>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-k>", ":m'<-2<CR>gv=gv")
 
 -- for diagnostic
 -- vim.keymap.set('n', '<leader>dq', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
